e6b23660fc154d73af3de35050a6900334539bb0
Felipe Walter Dafico Pfrimer <fpfirmer@gmail.com>
2021-11-29 21:52:03 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 echo "Iniciando instalação..."
 
 # Verificar tipo de boot BIOS ou EFI
-
+echo "Verificando tipo de boot"
 if [ -d "/sys/firmware/efi/efivars" ]
 then
-    echo "EFI." 
+    echo "Detectado EFI." 
 else
-    echo "BIOS."
+    echo "Detectado BIOS."
 fi
 
 # Habilitando NPT
