10d9d694fcc0d97bfae081e82d4c9e2bec832a0c
Felipe Walter Dafico Pfrimer <fpfirmer@gmail.com>
2021-11-29 21:31:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-echo "deu certo"
+#!/bin/bash
+
+echo "Iniciando instalação..."
+
+# Verificar conexão com a interent
+nc -z 8.8.8.8 53  >/dev/null 2>&1
+online=$?
+if [ $online -eq 0 ]; then
+    echo "Online"
+else
+    echo "Offline"
+fi
+
+
+# Habilitando NPT
+
