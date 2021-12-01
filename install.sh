d9fb538ff42222c3d59390968fb133c1930a6df9
Felipe Walter Dafico Pfrimer <fpfirmer@gmail.com>
2021-12-01 20:38:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,26 @@ echo "Verificando tipo de boot"
 if [ -d "/sys/firmware/efi/efivars" ]
 then
     echo "Detectado EFI." 
+    TIPO_BOOT = 1
 else
     echo "Detectado BIOS."
+    TIPO_BOOT = 0
 fi
 
 # Habilitando NPT
+timedatectl set-ntp true
 
+# Procura os discos do computador
+disk=()
+size=()
+name=()
+while IFS= read -r -d $'\0' device; do
+    device=${device/\/dev\//}
+    disk+=($device)
+    name+=("`cat "/sys/class/block/$device/device/model"`")
+    size+=("`cat "/sys/class/block/$device/size"`")
+done < <(find "/dev/" -regex '/dev/sd[a-z]\|/dev/vd[a-z]\|/dev/hd[a-z]' -print0)
+
+for i in `seq 0 $((${#disk[@]}-1))`; do
+    echo -e "${disk[$i]}\t${name[$i]}\t${size[$i]}"
+done
